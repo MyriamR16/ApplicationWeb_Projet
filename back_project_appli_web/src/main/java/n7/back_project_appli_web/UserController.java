b19bbe999d4a4649b5101aeba99a48f6ed119501
ikramberroug <ikramberroug24@gmail.com>
2025-05-23 17:23:52 +0200

    gestion token

--- back_project_appli_web/src/main/java/n7/back_project_appli_web/UserController.java
+++ back_project_appli_web/src/main/java/n7/back_project_appli_web/UserController.java
@@ -6,11 +6,11 @@ import java.util.Collection;
 import java.util.List;
 
 import java.util.Map;
+import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
